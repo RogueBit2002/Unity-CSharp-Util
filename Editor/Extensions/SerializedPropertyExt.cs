a65fde2bb5bf322bd6cd53dd7c07cd2e13462ce4
RogueBit2002 <laurens@kruis.name>
2022-08-21 18:35:49 +0200

    Added SerializedPropertyExt.GetDirectChildren()

--- Editor/Extensions/SerializedPropertyExt.cs
+++ Editor/Extensions/SerializedPropertyExt.cs
@@ -18,5 +18,26 @@ namespace LaurensKruis.CSharpUtil.Editor.Extensions
 				yield return property.GetArrayElementAtIndex(i);
 			}
 		}
+
+		public static IEnumerable<SerializedProperty> GetDirectChildren(this SerializedProperty property)
+        {
+			List<SerializedProperty> children = new List<SerializedProperty>();
+			if (!property.hasChildren)
+				return children;
+
+			string root = property.propertyPath;
+			SerializedProperty iterator = property.Copy();
+
+			iterator.NextVisible(true);
+			do
+			{
+				if (!iterator.propertyPath.StartsWith(root))
+					break;
+
+				children.Add(iterator.Copy());
+			} while (iterator.NextVisible(false));
+
+			return children;
+        }
     }
 }
